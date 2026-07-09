0e7dd939c00bed577105a705d0bec949ed58e14a
aarthi-06 <231001003@rajalakshmi.edu.in>
2026-07-09 12:27:31 +0530

    Delete week3_dn/src directory

--- week3_dn/src/main/java/com/cognizant/rest_api_demo/exception/CountryNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.cognizant.rest_api_demo.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Country not found")
-public class CountryNotFoundException extends Exception {
-}
